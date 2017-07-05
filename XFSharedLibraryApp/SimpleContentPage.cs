3b0cd431a57281b4fdc99c0f08cd7cf8caba3625
Nicolò Scarpa <nicolo.scarpa@freelands.it>
2017-07-05 11:00:26 +0200

    Building Your First Xamarin.Forms App

---
+++ XFSharedLibraryApp/SimpleContentPage.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace XFSharedLibraryApp
+{
+    public class SimpleContentPage : ContentPage
+    {
+        public SimpleContentPage()
+        {
+            var label1 = new Label
+            {
+                Text = "This is my label",
+                Font = Font.BoldSystemFontOfSize(NamedSize.Large)
+            };
+
+            var entry1 = new Entry
+            {
+                Placeholder = "Type something here"
+            };
+
+            Content = new StackLayout
+            {
+                Padding = 30,
+                Spacing = 10,
+                Children = {
+                    label1,
+                    entry1
+                }
+            };
+        }
+    }
+}
---
+++ XFSharedLibraryApp/SimpleXAMLContentPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace XFSharedLibraryApp
+{
+    public partial class SimpleXAMLContentPage : ContentPage
+    {
+        public SimpleXAMLContentPage()
+        {
+            InitializeComponent();
+
+            var label = new Label
+            {
+                Text = "This is my label",
+                FontSize = Device.GetNamedSize(NamedSize.Large, typeof(Label)),
+                FontAttributes = FontAttributes.Bold
+            };
+
+            var entry = new Entry
+            {
+                Placeholder = "Type something here"
+            };
+
+            var button = new Button
+            {
+                Text = "Click Me!"
+            };
+            button.Clicked += (sender, e) => {
+                label.Text = "User typed: " + entry.Text;
+            };
+
+            Content = new StackLayout {
+                Padding = 30,
+                Spacing = 10,
+                Children = {
+                    label,
+                    entry,
+                    button
+                }
+            };
+        }
+    }
+}
